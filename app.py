637cbfb8b8c5090ebe1e47703b3aa11748052f0a
williansbatista-spec <willian.s.batista@edu.pbh.gov.br>
2026-08-18 19:00:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -876,10 +876,14 @@ if not st.session_state.partida_iniciada:
 else:
     renderizar_painel_jogadores()
 
-    aluno = st.session_state.aluno_sorteado
-    tot_rodadas = st.session_state.get("total_rodadas", 20)
-    rodada_atual = st.session_state.rodada_atual
-    dc_atual = calcular_dificuldade_rodada(rodada_atual, tot_rodadas)
+    aluno = st.session_state.get("aluno_sorteado")
+tot_rodadas = st.session_state.get("total_rodadas", 20)
+rodada_atual = st.session_state.get("rodada_atual", 1)
+dc_atual = calcular_dificuldade_rodada(rodada_atual, tot_rodadas)
+
+# 2. Executa a validação do turno
+if aluno and rodada_atual < tot_rodadas - 1:
+    st.markdown(gerar_frase_convocacao(aluno))
 
     # Destaque para a Rodada e Progresso de Dificuldade (DC)
     col_info1, col_info2 = st.columns([2, 1])
