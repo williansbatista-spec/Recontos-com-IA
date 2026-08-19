6323e574ebfe2fd52ac772fb4ef6e4e6356ee742
williansbatista-spec <willian.s.batista@edu.pbh.gov.br>
2026-08-19 16:33:32 -0300

    Update app.py

--- app.py
+++ app.py
@@ -445,7 +445,7 @@ def gerar_imagem(prompt_text, together_key):
     if not together_key:
         return None
     try:
-        modelo_Qwen/Qwen-Image = st.session_state.get(
+        modelo_imagem = st.session_state.get(
             "Qwen/Qwen-Image", "Qwen/Qwen-Image"
         )
         url = "https://api.together.xyz/v1/images/generations"
